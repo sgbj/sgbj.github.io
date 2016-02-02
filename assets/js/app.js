0ed73711db027ec6de6432125afdb8dd201befaa
sgbj <scott.batary@gmail.com>
2016-02-01 20:23:04 -0600

    fixed ie issue and navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 // Make background image change position w/ mouse
+var sign = Math.sign || function(x) { return x > 0 ? 1 : x < 0 ? -1 : x; }
 var pattern = document.getElementById("image-pattern");
 document.addEventListener("mousemove", function (e) {
   var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   var ratioX = Math.sqrt(Math.pow(width / 2 - e.clientX, 2)) / Math.sqrt(Math.pow(width / 2, 2));
   var ratioY = Math.sqrt(Math.pow(height / 2 - e.clientY, 2)) / Math.sqrt(Math.pow(height / 2, 2));
-  pattern.setAttribute("x", -10 + 3 * ratioX * Math.sign(width / 2 - e.clientX) + "%");
-  pattern.setAttribute("y", 11 + 2 * ratioY * Math.sign(height / 2 - e.clientY) + "%");
+  pattern.setAttribute("x", -10 + 3 * ratioX * sign(width / 2 - e.clientX) + "%");
+  pattern.setAttribute("y", 11 + 2 * ratioY * sign(height / 2 - e.clientY) + "%");
 });
 
 var nav = document.querySelector("nav");
