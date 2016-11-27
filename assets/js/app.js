185fc8a51909f961ff08eb19201a4c6c97d1b46e
sgbj <scott.batary@gmail.com>
2016-11-26 21:25:49 -0600

    updating quotes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
 var quotes = [
+    'Software developer',
+    'Coder',
+    'Programmer',
     'Developer & designer',
     'Gamer',
     'App developer',
     'Blogger',
-    'Web developer'
+    'Web developer',
+    'Tinkerer'
 ];
 
 document.getElementById('quote').innerText = quotes[Math.floor(Math.random() * quotes.length)];
